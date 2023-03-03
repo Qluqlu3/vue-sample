e0ae8abcaa5b331f1f410c5adf6cf323d2891883
Qluqlu3 <plksmcgof@gmail.com>
2023-03-04 03:44:20 +0900

    vue first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "./components/MyForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    Form
+  }
+};
+</script>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" />
+      </div>
+      <div>
+        <label for="age">Age:</label>
+        <input type="number" id="age" v-model.number="age" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert(`Name: ${this.name}, Age: ${this.age}`);
+    }
+  }
+};
+</script>
